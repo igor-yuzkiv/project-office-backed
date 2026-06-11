fda17fed4be36b8ae7293dc04e4780074f169597
Igor Yuzkiv <igor97@gmail.com>
2026-06-11 18:20:19 +0300

    feat: update project links to use RouterLink and apply consistent styling for links

--- resources/js/pages/home/home/HomePage.vue
+++ resources/js/pages/home/home/HomePage.vue
@@ -1,21 +1,5 @@
-<script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import type { PaginatedResponse } from '@/shared/types'
-import { fetchTasksRequest } from '@/entities/task/api'
-import type { ITask } from '@/entities/task/types'
-
-const tasks = ref<PaginatedResponse<ITask> | null>(null)
-
-onMounted(async () => {
-    tasks.value = await fetchTasksRequest().catch((err) => {
-        console.error(err)
-        return null
-    })
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div class="p-2 flex-1 flex-col overflow-auto">
-        <pre>{{ tasks }}</pre>
-    </div>
+    <div class="p-2 flex-1 flex-col overflow-auto">not implemented yet</div>
 </template>
